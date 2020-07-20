25aa620bfa88712092e31508a0697dffc1ad7017
Blake.Dykes <blakedykes@ymail.com>
2020-07-19 22:08:51 -0500

    Module Three Commit: HTML to Templates with JSON

--- travlr/app.js
+++ travlr/app.js
@@ -8,6 +8,7 @@ const hbs = require('hbs');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const roomsRouter = require('./app_server/routes/rooms');
 
 const app = express();
 
@@ -18,15 +19,17 @@ app.set('view engine', 'hbs');
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
